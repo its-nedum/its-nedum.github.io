f7d1581a3fc1c93d41cc9502afc2e018f816e406
Chinedu Emesue <de.magnificent1@gmail.com>
2019-09-11 15:21:05 +0100

    mail

--- js/script.js
+++ js/script.js
@@ -1,18 +1,24 @@
-const mailer = () =>{
+$(document).ready(function() {
+    $('#contact-form').submit(function(e) {
+      const name    = document.getElementById('name')
+      const email   = document.getElementById('email')
+      const message = document.getElementById('message')
+  
+      if (!name.value || !email.value || !message.value) {
+        alert("Please check your entries");
+        return false;
+      } else {
+        $.ajax({
+          method: 'POST',
+          url: '//formspree.io/emesuechinedu@gmail.com',
+          data: $('#contact-form').serialize(),
+          datatype: 'json'
+        });
+        e.preventDefault();
+        $(this).get(0).reset();
+        alert("Message sent");
+      }
+    });
+  });
 
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
-
-Email.send({
-    SecureToken : "87228b4b-3b54-490d-86ac-d61a76a7ae15",
-    To : 'emesuechinedu@gmail.com',
-    From : `${name} - ${email}`,
-    Subject : "Hire Me Mail",
-    Body : `${message}`
-}).then(
-  message => alert(message)
-);
-
-}
 
