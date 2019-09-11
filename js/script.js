9ad28adfefeaa5999cb6cadbadfc3d6caebee495
Chinedu Emesue <de.magnificent1@gmail.com>
2019-09-11 15:46:15 +0100

    mail

--- js/script.js
+++ js/script.js
@@ -20,22 +20,3 @@ $(document).ready(function() {
       }
     });
   });
-
-// const mailer = () =>{
-
-//     const name = document.getElementById('name').value;
-//     const email = document.getElementById('email').value;
-//     const message = document.getElementById('message').value;
-
-// Email.send({
-//     SecureToken : "87228b4b-3b54-490d-86ac-d61a76a7ae15",
-//     To : 'emesuechinedu@gmail.com',
-//     From : `${name} - ${email}`,
-//     Subject : "Hire Me Mail",
-//     Body : `${message}`
-// }).then(
-//   message => alert(message)
-// );
-
-// }
-
